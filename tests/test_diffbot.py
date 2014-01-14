51fb750b6f1edf218b70297ab3bd0e08637bd24e
Attila Oláh <attilaolah@gmail.com>
2014-01-14 13:18:23 +0700

    add some unit tests

---
+++ tests/test_diffbot.py
@@ -0,0 +1,72 @@
+"""Diffbot API tests."""
+import json
+import os.path
+import unittest
+import urlparse
+
+import mock
+
+import diffbot
+
+
+TOKEN = 'test'
+
+GITHUB_COM = 'https://github.com'
+
+
+def fake_requests_get(url, params=None):
+    """A stub requests.get() implementation."""
+    if params['token'] != TOKEN:
+        raise ValueError(u'Invalid token: {}'.format(params['token']))
+    api = urlparse.urlparse(url)
+    url = urlparse.urlparse(params['url'])
+    resource = os.path.join('tests', 'resources', url.netloc,
+                            api.path.strip('/') + '.json')
+    with open(resource, 'rb') as src:
+        return FakeResponse(json.load(src))
+
+
+class FakeResponse(object):
+    """A stub `requests.Response` implementation."""
+
+    def __init__(self, json_data):
+        """Set up the json data."""
+        self._json = json_data
+
+    def json(self):
+        """Return the JSON data."""
+        return self._json
+
+
+class ClientTestCase(unittest.TestCase):
+    """Test case for the client methods."""
+
+    def setUp(self):
+        """Set up a mock patcher."""
+        self.patcher = mock.patch('requests.get', fake_requests_get)
+        self.patcher.start()
+        self.client = diffbot.Client(token=TOKEN)
+        self.client_v1 = diffbot.Client(token=TOKEN, version=1)
+        self.client_v2 = diffbot.Client(token=TOKEN, version=2)
+
+    def tearDown(self):
+        """Stop the patcher."""
+        self.patcher.stop()
+
+    def test_article(self):
+        """Test the Article API."""
+        result = self.client.article(GITHUB_COM)
+        self.assertEqual(result['url'], GITHUB_COM)
+        self.assertEqual(result['title'], 'Build software better, together.')
+
+    def test_article_v1(self):
+        """Test the Article API."""
+        result = self.client_v1.article(GITHUB_COM)
+        self.assertEqual(result['url'], GITHUB_COM)
+        self.assertEqual(result['title'], 'Build software better, together.')
+
+    def test_article_v2(self):
+        """Test the Article API."""
+        result = self.client_v2.article(GITHUB_COM)
+        self.assertEqual(result['url'], GITHUB_COM)
+        self.assertEqual(result['title'], 'Build software better, together.')
